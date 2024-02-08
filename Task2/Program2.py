70a758a70a2160f2aaaf8c636e9d7970460a4bf6
Radhapr296 <radhapurohit96@gmail.com>
2024-02-08 19:24:53 +0530

    project

---
+++ Task2/Program2.py
@@ -0,0 +1,9 @@
+#Create a program that takes two numbers as input and prints whether the first number is greater than, less than, or equal to the second number.
+num1 = int(input("Enter First number"))
+num2 = int(input("Enter Second number"))
+if num1 == num2:
+    print("num1 is equal to num2")
+if num1 > num2:
+    print("num1 is greater than num2")
+if num1 < num2:
+    print("num1 is lesser than num2")
---
+++ Task2/Program3.py
@@ -0,0 +1,7 @@
+#Develop a Python script that calculates the square and cube of a given number.
+
+num = int(input("Enter the number"))
+sqr_num = num * num
+cube_num = num * num * num
+print(sqr_num)
+print(cube_num)
